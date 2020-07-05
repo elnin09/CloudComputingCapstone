b5b9a33c59632b0a5cd79e00a32dc12722778f44
Swapnil Darmora <swapnildarmora@gmail.com>
2020-07-05 14:49:58 +0530

    Update queries3.sql

--- Task01/queries3.sql
+++ Task01/queries3.sql
@@ -40,36 +40,8 @@ select T1.origin as origin1, T1.Destination as destination1, T2.origin as origin
  T2.UniqueCarrier as UniqueCarrier2 , T2.flightnum as FlightNumner2,
  T1.departuretime as DepartureTime1, T2.departuretime as DepartureTime2
  from FirstLeg T1, Secondleg T2
- where T1.destination = T2.origin and datediff(T2.flightdate,T1.Flightdate) = 2
- /*and 
- (
-     (T1.origin = 'CMI' and T1.destination = 'ORD' and T2.destination = 'LAX')
-     OR
-     (T1.origin = 'JAX' and T1.destination = 'DFW' and T2.destination = 'CRP')
- ) */
- ;
+ where T1.destination = T2.origin and datediff(T2.flightdate,T1.Flightdate) = 2;
  
-/*CMI → ORD → LAX, 04/03/2008
-JAX → DFW → CRP, 09/09/2008
-SLC → BFL → LAX, 01/04/2008
-LAX → SFO → PHX, 12/07/2008
-DFW → ORD → DFW, 10/06/2008
-LAX → ORD → JFK, 01/01/2008*/
-
-select * from output3_2
-where 
-(origin1 = 'CMI' and  destination1 = 'ORD' and destination2 = 'LAX' and Flightdate1 = '2008-03-04')
-OR
-(origin1 = 'JAX' and  destination1 = 'DFW' and destination2 = 'CRP' and Flightdate1 = '2008-09-09')
-OR
-(origin1 = 'SLC' and  destination1 = 'BFL' and destination2 = 'LAX' and Flightdate1 = '2008-04-01')
-OR
-(origin1 = 'LAX' and  destination1 = 'SFO' and destination2 = 'PHX' and Flightdate1 = '2008-07-12')
-OR
-(origin1 = 'DFW' and  destination1 = 'ORD' and destination2 = 'DFW' and Flightdate1 = '2008-06-10')
-OR
-(origin1 = 'LAX' and  destination1 = 'ORD' and destination2 = 'JFK' and Flightdate1 = '2008-01-01')
-;
 
 
 
